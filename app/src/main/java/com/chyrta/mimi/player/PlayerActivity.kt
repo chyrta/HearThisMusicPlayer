ff58eea2f3bee37d6f1f6fd4fb050209ffea0815
Dzmitry Chyrta <d.chyrta@datarockets.com>
2019-07-05 06:15:19 +0300

    Add module to Player activity

--- app/src/main/java/com/chyrta/mimi/player/PlayerActivity.kt
+++ app/src/main/java/com/chyrta/mimi/player/PlayerActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v4.media.session.PlaybackStateCompat
 import android.text.format.DateUtils
+import android.util.Log
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
@@ -139,37 +140,37 @@ class PlayerActivity : AppCompatActivity(), CurrentSessionCallback, SeekBar.OnSe
 
     override fun onStopTrackingTouch(seekbar: SeekBar?) {
         draggingScrubber = false
-        if (streamingManager?.mLastPlaybackState != null) {
-            streamingManager?.onSeekTo(seekbar!!.progress)
+        if (streamingManager.mLastPlaybackState != null) {
+            streamingManager.onSeekTo(seekbar!!.progress)
         } else {
             seekbar!!.progress = 0
         }
     }
 
     private fun onPlayPauseButton() {
-        if (streamingManager?.isPlaying!!) {
-            streamingManager?.onPause()
+        if (streamingManager.isPlaying) {
+            streamingManager.onPause()
         } else {
-            streamingManager?.onPlay(currentSong)
+            streamingManager.onPlay(currentSong)
         }
     }
 
     private fun seekBy(seconds: Int) {
-        if (streamingManager?.isPlaying!!) {
+        if (streamingManager.isPlaying) {
             val newPosition = seekBar.progress + seconds * 1000
-            streamingManager?.onSeekTo(newPosition)
+            streamingManager.onSeekTo(newPosition)
         }
     }
 
     private fun configureAudioStreaming() {
-        streamingManager?.setShowPlayerNotification(true)
-        streamingManager?.setPendingIntentAct(getNotificationPendingIntent())
+        streamingManager.setShowPlayerNotification(true)
+        streamingManager.setPendingIntentAct(getNotificationPendingIntent())
     }
 
     private fun configurePlayer() {
-        if (streamingManager?.isPlaying!! && selectedMediaMetaData == null) {
-            currentSong?.playState = streamingManager?.mLastPlaybackState!!
-            currentSong = streamingManager?.currentAudio
+        if (streamingManager.isPlaying && selectedMediaMetaData == null) {
+            currentSong?.playState = streamingManager.mLastPlaybackState
+            currentSong = streamingManager.currentAudio
         } else {
             currentSong = selectedMediaMetaData
             streamingManager?.onPlay(currentSong)
@@ -188,16 +189,16 @@ class PlayerActivity : AppCompatActivity(), CurrentSessionCallback, SeekBar.OnSe
 
     override fun onStart() {
         super.onStart()
-        streamingManager?.subscribesCallBack(this)
+        streamingManager.subscribesCallBack(this)
     }
 
     override fun onStop() {
-        streamingManager?.unSubscribeCallBack()
+        streamingManager.unSubscribeCallBack()
         super.onStop()
     }
 
     override fun onDestroy() {
-        streamingManager?.unSubscribeCallBack()
+        streamingManager.unSubscribeCallBack()
         super.onDestroy()
     }
 
