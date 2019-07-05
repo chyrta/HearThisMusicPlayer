23c7ccae4a77673d33e2bd4edfc5543ffc09aa4c
Dzmitry Chyrta <d.chyrta@datarockets.com>
2019-07-05 04:53:11 +0300

    Update formatting of base classes

--- app/src/main/java/com/chyrta/mimi/base/MviView.kt
+++ app/src/main/java/com/chyrta/mimi/base/MviView.kt
@@ -2,7 +2,7 @@ package com.chyrta.converter.base
 
 import io.reactivex.Observable
 
-interface MviView<I: MviIntent, in S: MviViewState> {
+interface MviView<I : MviIntent, in S : MviViewState> {
     fun intents(): Observable<I>
     fun render(state: S)
 }
--- app/src/main/java/com/chyrta/mimi/base/MviViewModel.kt
+++ app/src/main/java/com/chyrta/mimi/base/MviViewModel.kt
@@ -2,7 +2,7 @@ package com.chyrta.converter.base
 
 import io.reactivex.Observable
 
-interface MviViewModel<I: MviIntent, S: MviViewState> {
+interface MviViewModel<I : MviIntent, S : MviViewState> {
     fun processIntents(intents: Observable<I>)
     fun states(): Observable<S>
 }
